0b6decf492e7ee0e626d4e10b2671074bdaaaa93
GustavoDantasSantos <guvinidantas@gmail.com>
2024-06-06 11:19:16 -0300

    first commit

---
+++ src/main/java/com/gustavo/br/crud_rockeseat_desafio01/CrudRockeseatDesafio01Application.java
@@ -0,0 +1,13 @@
+package com.gustavo.br.crud_rockeseat_desafio01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudRockeseatDesafio01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudRockeseatDesafio01Application.class, args);
+	}
+
+}
